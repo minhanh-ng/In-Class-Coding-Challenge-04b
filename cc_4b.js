d38de38093cb37ec9e3de3c0c62c55845e77ecb8
Minh Anh Nguyen <minhanhn@usf.edu>
2025-09-17 17:24:28 -0400

    define array in js file

---
+++ cc_4b.js
@@ -0,0 +1,7 @@
+const product = [
+    {sku: "SKU-001", name:"Lipstick", category: "makeup", price: 40.50, inventory: 50},
+    {sku: "SKU-002", name: "Mascara", category: "makeup", price: 30, inventory: 100},
+    {sku: "SKU-003", name: "Laptop", category: "electronics", price: 1000, inventory: 150},
+    {sku: "SKU-004", name: "Carpet", category: "household", price: 100, inventory: 3},
+    {sku: "SKU-005", name: "Jacket", category: "apparel", price: 200, inventory: 200}
+];
